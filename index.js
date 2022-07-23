163f053e3689be9b461fadadb0a6633d1cd74a9f
ManiBharathi13 <manibsaran@gmail.com>
2022-07-23 13:12:30 +0530

    Axios added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const https = require('https');
+const axios = require('axios');
 
 try{
     const name = core.getInput('name')
@@ -15,25 +16,34 @@ try{
     let startTime = new Date()
     console.log("Polling Start Time : "+  startTime.toGMTString())
     let pollCount =0 
-   const pollInterval =  setInterval(()=>{
-        const req = https.request(options, res => {
-        console.log(`Poll ${++pollCount} - statusCode: ${res.statusCode}`);  
+   const pollInterval =  setInterval(function(){
+        pollCount++
+        
+        axios
+        .get('https://fantasy.premierleague.com/api/bootstrap-static/')
+        .then(res => {
+          console.log(`Poll ${pollCount} - statusCode: ${res.status}`);
+          
+        })
+        .catch(error => {
+          console.error(error);
         });
-        req.on('error', error => {
-            console.error(error);
-          });
         if((new Date() - startTime)/ 1000 >= 60)
         {
             const response = "Success"
-            core.setOutput("response", response)
+           core.setOutput("response", response)
             clearInterval(pollInterval)
             console.log("Polling Complete")
+            process.exit()
         }
     },5000)
+
+   
 }
 catch(err)
 {
     console.log(err)
     core.setFailed(err.message);
     core.setOutput("response", "Failure")
+    process.exit()
 }
